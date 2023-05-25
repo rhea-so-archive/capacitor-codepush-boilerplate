5afe075334307ada59b5d8403fc4d9974b9188e0
rhea-so <jeonghyeon.rhea@gmail.com>
2023-05-26 01:29:59 +0900

    🐛 fix: fix app version sync import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import AppVersionSync from "./VersionController";
+AppVersionSync();
+
 import React from "react";
 import GlobalStyle from "@src/GlobalStyle";
 import HiThere from "./Components/HiThere";
-import AppVersionSync from "./VersionController";
 
 export default async function Index() {
-  await AppVersionSync();
-
   return (
     <>
       <GlobalStyle />
